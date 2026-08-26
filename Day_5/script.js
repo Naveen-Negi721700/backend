c5d271c2a036e7bbbf924801e5ad35c45f189b5d
Naveen Negi <naveen72170@gmail.com>
2026-08-26 21:43:53 +0530

    practicing javascript basics

---
+++ Day_5/script.js
@@ -0,0 +1,30 @@
+// console.log("hello world")
+
+
+// var a=4;
+// var b=5;
+
+// console.log(a+b);
+
+
+// function nice(name) {
+//     console.log(`your + ${name} + good`);
+    
+// }
+
+// nice("naveen");
+// nice("rohan")
+
+
+// let arr=[5,8,7,9,5,6];
+
+// let newarr= arr.map((e)=>{
+//     return e**2;
+// })
+
+// console.log(newarr);
+
+
+document.querySelector(".box").outerHTML=<div>
+    hey i am naveen
+</div>
