98bb8f626216f5a2540842c3050d74167cf0190e
Naveen Negi <naveen72170@gmail.com>
2026-08-27 20:17:38 +0530

    utlit apierrors

--- Day_4/src/app.js
+++ Day_4/src/app.js
@@ -3,6 +3,14 @@ import cors from "cors"
 import express from "express"
 const app=express();
 
+app.use(cors({
+    origin: Process.env.CORS_ORIGIN,
+    credentials:true,
+}))
 
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export default app;
